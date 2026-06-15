2cda6660ae3409d29c38847c0d06acd2c1caca1f
Tanzeem <tanzeemafsar74@gmail.com>
2026-06-15 23:38:04 +0600

    added all query

--- Query.sql
+++ Query.sql
@@ -14,3 +14,18 @@ where payment_status is null;
 select booking_id,full_name,fixture,total_cost
 from bookings inner join users on bookings.user_id=users.user_id
 inner join matches on bookings.match_id=matches.match_id;
+
+select users.user_id,full_name,booking_id
+from users left join bookings
+on users.user_id=bookings.user_id;
+
+
+select booking_id,match_id,total_cost
+from bookings where total_cost > (
+  select avg(total_cost) from bookings
+);
+
+
+select match_id, fixture, base_ticket_price
+from matches order by base_ticket_price desc
+offset 1 limit 2;
