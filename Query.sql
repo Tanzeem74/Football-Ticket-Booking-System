23b0251a2075fcc8717e82c991db33a6b92db494
Tanzeem <tanzeemafsar74@gmail.com>
2026-06-15 23:36:24 +0600

    added query three and four

--- Query.sql
+++ Query.sql
@@ -4,3 +4,13 @@ where tournament_category='Champions League' and match_status='Available';
 
 select user_id,full_name,email from users
 where full_name ilike 'Tanvir%' or full_name ilike '%Haque%'
+
+
+select booking_id, user_id, match_id,
+coalesce(payment_status,'Action Required') as systematic_status
+from bookings
+where payment_status is null;
+
+select booking_id,full_name,fixture,total_cost
+from bookings inner join users on bookings.user_id=users.user_id
+inner join matches on bookings.match_id=matches.match_id;
